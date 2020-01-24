6accad59e4cef39f12e9db7d28a04e77129efcd7
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-24 09:23:36 +0100

    Changed some classes names

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 $(document).ready(function () {
   console.log('hello world');
 });
+
+
+// to do
+// click on a user, select it, show its chat
+// send a message to that specific user and get a random reply
+// show and hide the option menu on bubble chat
+// search feature
